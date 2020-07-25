6880f74797c3473b9a997904e4674777cd1e7a04
Pablo Ramon Varela de Souza <pabloramon044@gmail.com>
2020-07-25 10:58:56 -0300

    salvando

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Teste das funcionalidades do pyChomikBox
+import os
 
 from ChomikBox import *
 
@@ -25,6 +26,46 @@ class Usuario:
         self.logado = False
         return self.logado
 
-user = Usuario('pabloramon044', 'Pablo_Ramon044')
-print(user.esta_logado())
-user.login()
+    # Retorna a lista com todas as pastas da conta logada
+    def listar_pastas(self):
+        if self.logado:
+            pastas = []
+            root = self.Chomik.folders_list()
+            #self.caminhar(pastas, root[2])
+            for pasta in root:
+                self.caminhar(pastas, pasta)
+            return pastas
+        # Retornar lista vazia se não estiver logado
+        return []
+
+    # Método privado para andar na lista de diretórios do chomikuj
+    def caminhar(self, lista_pastas, pasta_chomik):
+        # Adicionar pasta atual a lista de pastas global
+        lista_pastas.append(pasta_chomik)
+        # Para cada pasta dentro dela faça esse método novamente
+        # Se não houver pastas somente retorne
+        for pasta in pasta_chomik.folders_list():
+            self.caminhar(lista_pastas, pasta)
+        return
+
+
+def salvar_pastas_arquivo(nome_arquivo, lista_pastas):
+    arquivo = open(nome_arquivo, 'a')
+    for pasta in lista_pastas:
+        linha = '{},{},{},{},{},{},{},{}\n'.format(pasta.chomik, pasta.folder_id, pasta.name, pasta.parent_folder, pasta.hidden, pasta.adult, pasta.gallery_view, pasta.password)
+        arquivo.write(linha)
+    arquivo.close()
+
+def atualizar_pastas_arquivo(nome_arquivo):
+    pass
+
+# TODO Melhorar a escrita do arquivo salvo e dar uma olhada no folder cache do chomikbox para ver o que é.
+if __name__ == '__main__':
+    user = Usuario('pabloramon044', 'Pablo_Ramon044')
+    print(user.esta_logado())
+    user.login()
+    pastas_chomik = user.listar_pastas()
+    print(pastas_chomik)
+    print(len(pastas_chomik))
+    salvar_pastas_arquivo('pastas_chomikuj', pastas_chomik)
+    print('Finalizado...')
